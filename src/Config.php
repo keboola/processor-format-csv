43a5d080614acaa28b5de3d7252a7e61bf67d27c
Tomáš Fejfar <tomas.fejfar@gmail.com>
2018-03-09 15:10:43 +0000

    Remove unused getters from config

--- src/Config.php
+++ src/Config.php
@@ -8,16 +8,6 @@ use Keboola\Component\Config\BaseConfig;
 
 class Config extends BaseConfig
 {
-    public function getDelimiterFrom(): string
-    {
-        return $this->getValue(['parameters', 'delimiterFrom']);
-    }
-
-    public function getEnclosureFrom(): string
-    {
-        return $this->getValue(['parameters', 'enclosureFrom']);
-    }
-
     public function getDelimiterTo(): string
     {
         return $this->getValue(['parameters', 'delimiterTo']);
