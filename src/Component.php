75e67d2fc11ba7362d4b199ac32db7564d8147bb
Tomáš Fejfar <tomas.fejfar@gmail.com>
2018-03-09 15:29:21 +0000

    It needs to catch throwable to catch a missing file!

--- src/Component.php
+++ src/Component.php
@@ -27,7 +27,7 @@ class Component extends BaseComponent
         foreach ($tablesFinder as $csvTableFrom) {
             try {
                 $tableManifest = $this->getManifestManager()->getTableManifest($csvTableFrom->getFilename());
-            } catch (InvalidArgumentException $e) {
+            } catch (Throwable $e) {
                 throw new UserException(
                     'This processor needs table manifest to work. Add a Create Manifest processor before it.',
                     0,
