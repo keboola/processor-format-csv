da0b11d63acd20251284a966a87b4c47f49d4fcd
Tomáš Fejfar <tomas.fejfar@gmail.com>
2018-03-09 13:18:57 +0000

    Enclosure may be empty string

--- src/ConfigDefinition.php
+++ src/ConfigDefinition.php
@@ -24,7 +24,6 @@ class ConfigDefinition extends BaseConfigDefinition
                 ->end()
                 ->scalarNode('enclosureTo')
                     ->isRequired()
-                    ->cannotBeEmpty()
                 ->end()
             ->end()
         ;
